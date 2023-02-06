e8088779beb1f2f984fefead0e3fdf87bbff84b8
thuydiem1702 <thuydiem17022001@gmail.com>
2023-02-06 16:16:35 +0700

    up

--- case_study/case-study/src/app/app.module.ts
+++ case_study/case-study/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HearderComponent } from './component/hearder/hearder.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './component/home/home.component';
+import { FacilityListComponent } from './component/facility/facility-list/facility-list.component';
+import { FacilityEditComponent } from './component/facility/facility-edit/facility-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HearderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    FacilityListComponent,
+    FacilityEditComponent
   ],
   imports: [
     BrowserModule,
