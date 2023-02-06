24003ade827deaf7511a4f96e0ccfe04d2145792
thuydiem1702 <thuydiem17022001@gmail.com>
2023-02-06 14:28:46 +0700

    up

---
+++ case_study/case-study/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HearderComponent } from './component/hearder/hearder.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HearderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
