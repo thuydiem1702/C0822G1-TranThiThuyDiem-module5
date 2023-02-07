a74e159c3a9e0e1488b0f500a2d6e25aadb6c244
thuydiem1702 <thuydiem17022001@gmail.com>
2023-02-07 22:09:22 +0700

    up

--- case_study/case-study/src/app/app-routing.module.ts
+++ case_study/case-study/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import {CustomerListComponent} from './component/customer/customer-list/customer
 import {ContractListComponent} from './component/contract/contract-list/contract-list.component';
 import {FacilityCreateComponent} from './component/facility/facility-create/facility-create.component';
 import {FacilityEditComponent} from './component/facility/facility-edit/facility-edit.component';
+import {CustomerEditComponent} from './component/customer/customer-edit/customer-edit.component';
+import {CustomerCreateComponent} from './component/customer/customer-create/customer-create.component';
 
 
 const routes: Routes = [
@@ -16,6 +18,8 @@ const routes: Routes = [
   {path: 'createFacility', component: FacilityCreateComponent},
   {path: 'editFacility', component: FacilityEditComponent},
   {path: 'customer', component: CustomerListComponent},
+  {path: 'createCustomer', component: CustomerCreateComponent},
+  {path: 'editCustomer', component: CustomerEditComponent},
   {path: 'contract', component: ContractListComponent},
 ];
 
